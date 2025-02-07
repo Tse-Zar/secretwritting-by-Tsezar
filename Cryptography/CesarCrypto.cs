031a6b09c15301426b2143405390cfa107b23ca1
Tse-Zar <mashkovfebor@gmail.com>
2025-02-07 15:59:56 +0700

    FirstUpdate
    fffirst

---
+++ Cryptography/CesarCrypto.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptography
+{
+    class CesarCrypto(string input, int step = 1)
+    {
+        private readonly static char[] charsRuVariable = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".ToCharArray();
+        private readonly int charsRuValue = charsRuVariable.Length;
+
+        private readonly static char[] charsEngVariable = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        private readonly int charsEngValue = charsEngVariable.Length;
+        public void CezarCrypt()
+        {
+            string resultTemp = "";
+            foreach (var c in input)
+            {
+                if (char.IsLetter(c))
+                {
+                    char lowerChar = char.ToLower(c);
+                    int index = Array.IndexOf(charsRuVariable, lowerChar);
+
+                    if (index >= 0)
+                    {
+                        int newIndex = (index + step) % charsRuValue;
+
+                        if (newIndex < 0)
+                        {
+                            newIndex += charsRuValue;
+                        }
+                   
+                        char newChar = charsRuVariable[newIndex];
+                        resultTemp += char.IsUpper(c) ? char.ToUpper(newChar) : newChar;
+                    }
+                }
+                else
+                    resultTemp += c;
+            }
+
+
+            Console.WriteLine(resultTemp);
+
+        }
+
+        public void CezarEncrypt()
+        {
+            string resultTemp = "";
+            foreach (var c in input)
+            {
+                if (char.IsLetter(c))
+                {
+                    char lowerChar = char.ToLower(c);
+                    int index = Array.IndexOf(charsRuVariable, lowerChar);
+
+                    if (index >= 0)
+                    {
+                        int newIndex = (index - step) % charsRuValue;
+
+                        if (newIndex < 0)
+                        {
+                            newIndex += charsRuValue;
+                        }
+
+                        char newChar = charsRuVariable[newIndex];
+                        resultTemp += char.IsUpper(c) ? char.ToUpper(newChar) : newChar;
+                    }
+                }
+                else
+                    resultTemp += c;
+            }
+
+
+            Console.Write(resultTemp);
+
+        }
+    }
+}
---
+++ Cryptography/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace Cryptography
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("╔════╗╔══╗╔═══╗╔═══╗╔══╗╔═══╗╔══╗───╔══╗╔═══╗╔╗╔╗╔═══╗╔════╗╔══╗\n" +
+                              "╚═╗╔═╝║╔═╝║╔══╝╚═╗─║║╔╗║║╔═╗║║╔═╝───║╔═╝║╔═╗║║║║║║╔═╗║╚═╗╔═╝║╔╗║\n" +
+                              "──║║──║╚═╗║╚══╗─╔╝╔╝║╚╝║║╚═╝║║╚═╗───║║──║╚═╝║║╚╝║║╚═╝║──║║──║║║║\n" +
+                              "──║║──╚═╗║║╔══╝╔╝╔╝─║╔╗║║╔╗╔╝╚═╗║───║║──║╔╗╔╝╚═╗║║╔══╝──║║──║║║║\n" +
+                              "──║║──╔═╝║║╚══╗║─╚═╗║║║║║║║║─╔═╝║───║╚═╗║║║║──╔╝║║║─────║║──║╚╝║\n" +
+                              "──╚╝──╚══╝╚═══╝╚═══╝╚╝╚╝╚╝╚╝─╚══╝───╚══╝╚╝╚╝──╚═╝╚╝─────╚╝──╚══╝");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            try
+            {
+                CesarCrypto crypto = new("");
+                while (true)
+                {
+                    Console.Write("Кодирование/Декодирование (C/E) --> ");
+                    string? input = Console.ReadLine();
+                    if (input == "C")
+                    {
+                        Console.Write("Хорошо, выберете режим шифрования из доступных (С/0) --> ");
+                        input = Console.ReadLine();
+
+                        switch (input)
+                        {
+                            case "C":
+                                Console.Write("Хороший выбор! Выберет размер шага (целое число, 0 - перебор всех вариантов) --> ");
+                                int step = Convert.ToInt32(Console.ReadLine());
+
+                                Console.Write("Введите текст для шифрования или укажите путь к файлу с текстом --> ");
+                                input = Console.ReadLine();
+
+                                Console.WriteLine("Принято! Ждите результат...");
+                                if (input != null)
+                                    crypto = new(input, step);
+
+                                crypto.CezarCrypt();
+
+                                break;
+                            case "0":
+                                Console.Write("Вы действительно хотите выйти из программы? (Д/н)    ");
+                                string? c = Console.ReadLine();
+                                Console.WriteLine("Принято...");
+                                if (c == "н")
+                                {
+                                    continue;
+                                }
+                                else if (c == "Д")
+                                {
+                                    Environment.Exit(0);
+                                }
+                                break;
+                        }
+                    }
+                    else if (input == "E")
+                    {
+                        Console.Write("Хорошо, выберете режим дешифрования из доступных (С/0) --> ");
+                        input = Console.ReadLine();
+
+                        switch (input)
+                        {
+                            case "C":
+                                Console.Write("Хороший выбор! Выберет размер шага (целое число, 0 - перебор всех вариантов) --> ");
+                                int step = Convert.ToInt32(Console.ReadLine());
+
+                                Console.Write("Введите текст для дешифрования или укажите путь к файлу с текстом --> ");
+                                input = Console.ReadLine();
+
+                                Console.WriteLine("Принято! Ждите результат...");
+                                if (input != null)
+                                    crypto = new(input, step);
+
+                                crypto.CezarEncrypt();
+
+                                break;
+                            case "0":
+                                Console.Write("Вы действительно хотите выйти из программы? (Д/н)    ");
+                                string? c = Console.ReadLine();
+                                Console.WriteLine("Принято...");
+                                if (c == "н")
+                                {
+                                    continue;
+                                }
+                                else if (c == "Д")
+                                {
+                                    Environment.Exit(0);
+                                }
+                                break;
+                        }
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
